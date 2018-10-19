05ba2e1ab30204d77200c037de3696b64f6ea4fb
Daniëlle <danielle.de.kloet@hva.nl>
2018-10-19 17:30:31 +0200

    Spatie

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
-/*eslint-env browser*/
-/*eslint 'no-console':0*/
 
+//declareren van vars en elementen in de dom
 var article = document.querySelector('section > article');
-var check = document.querySelector('footer > button');
+var checkbutton = document.querySelector('article > footer > button');
+//console.log("button? ",checkbutton);
+
+checkbutton.addEventListener('click', function() {
+    //console.log("we hebben een klik dames en heren");
+    checkbutton.classList.toggle("show");
+
+})
+
+
+
+
+/*
+
 
 check.addEventListener('click', function() {
                        article.classList.add('show');
@@ -15,3 +26,4 @@ var form = document.querySelector('li > img');
 form.addEventListener('click', function() {
     form.classList.add('down');
 })
+*/
